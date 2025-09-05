60f046e6547d17e9ede8096a762fc8a3f534c43c
Foodevn <hoangphuc0918065630@gmail.com>
2025-09-05 11:19:22 +0700

    Authentication setup
    Signed-off-by: Foodevn <hoangphuc0918065630@gmail.com>

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,11 +1,7 @@
 export default {
     providers: [
         {
-            // Replace with your own Clerk Issuer URL from your "convex" JWT template
-            // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
-            // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
-            // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-            domain: "https://known-kangaroo-26.clerk.accounts.dev",
+            domain: "https://national-rooster-52.clerk.accounts.dev",
             applicationID: "convex",
         },
     ]
--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { FullscreenLoader } from "./fullscreen-loader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -15,16 +16,15 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
                 client={convex}
             >
                 <Authenticated>
-                    <h1>Welcome to the app! </h1>
+                    {children}
                 </Authenticated>
                 <Unauthenticated>
-                    <p>Please log in</p>
                     <div className="flex flex-col items-center justify-center min-h-screen">
                         <SignIn />
                     </div>
                 </Unauthenticated>
                 <AuthLoading>
-                    <p>Loading auth...</p>
+                    <FullscreenLoader label="Loading authentication..." />
                 </AuthLoading>
             </ConvexProviderWithClerk>
 

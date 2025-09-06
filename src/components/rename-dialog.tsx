784feb7b5436c2b171f8ebd77711b4add52c631d
nhquang2509 <nguyenhuuquangmss@gmail.com>
2025-09-06 20:57:55 +0700

    Organizations feature
    Signed-off-by: nhquang2509 <nguyenhuuquangmss@gmail.com>

--- src/components/rename-dialog.tsx
+++ src/components/rename-dialog.tsx
@@ -14,6 +14,7 @@ import { Input } from "./ui/input";
 import { Id } from "../../convex/_generated/dataModel";
 import { api } from "../../convex/_generated/api";
 import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 interface RenameDialogProps {
   documentId: Id<"documents">;
@@ -32,6 +33,8 @@ export const RenameDialog = ({ documentId, initialTitle, children }: RenameDialo
         e.preventDefault();
         setIsUpdating(true);
         update({ id: documentId, title: title.trim() || "Untitled"  })
+        .catch(() => toast.error("Something went wrong"))
+        .then(() => toast.success("Document updated"))
         .finally(() => {
             setIsUpdating(false);
             setOpen(false);

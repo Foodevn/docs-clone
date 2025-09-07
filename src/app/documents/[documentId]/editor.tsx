91e0875a502b7bd54ff17ed8e7fe305a6989aae1
Foodevn <hoangphuc0918065630@gmail.com>
2025-09-07 10:05:02 +0700

    feat: integrate Liveblocks for collaborative editing and add threads functionality
    - Added Liveblocks client and React packages to package.json for real-time collaboration.
    - Implemented Room component to manage Liveblocks context in the document editor.
    - Created Threads component to display and manage collaborative threads in the editor.
    - Updated Editor component to include Threads and Liveblocks extensions.
    - Refactored DocumentIdPage to wrap Editor with Room for Liveblocks context.
    - Enhanced Toolbar with new comment functionality for collaborative editing.
    - Improved global styles for responsive thread display in the editor.
    - Added necessary CSS imports for Liveblocks UI components.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -16,88 +16,97 @@ import ImageResize from "tiptap-extension-resize-image"
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
-import {EditorContent, useEditor} from '@tiptap/react'
+import { EditorContent, useEditor } from '@tiptap/react'
 
-import {useEditorStore} from "@/store/use-editor-store";
+import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height';
 import { Line } from 'recharts';
 import { Ruler } from './ruler';
 
-export const Editor = () => {
-  const { setEditor } = useEditorStore();
-    const editor = useEditor({
-      onCreate({ editor }) {
-        setEditor(editor);
-      },
-      onDestroy() {
-          setEditor(null); 
-      },
-      onUpdate({ editor }) {
-          setEditor(editor);
-      },
-      onSelectionUpdate({ editor }) {
-          setEditor(editor);
-      },
-      onTransaction({ editor }) {
-          setEditor(editor);
-      },
-      onFocus({ editor }) {
-          setEditor(editor);
-      },
-      onBlur({ editor }) {
-          setEditor(editor);
-      },
-      onContentError({ editor }) {
-          setEditor(editor);
-      },
-      editorProps: {
-        attributes: {
-          style: "padding-left: 56px; padding-right: 56px;",
-          class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor text",
-        },
-      },
-        extensions: [
-          StarterKit,
-          LineHeightExtension.configure({
-            types: ["heading", "paragraph"],
-            defaultLineHeight: "normal"
-          }),
-          FontSizeExtension,
-          TextAlign.configure({
-            types: ["heading", "paragraph"],
-          }),
-          Link.configure({
-            openOnClick: false,
-            autolink: true,
-            defaultProtocol: "https"
+import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
+import { Threads } from './threads';
 
-          }),
-          Color,
-          Highlight.configure({
-            multicolor: true,
-          }),
-          TextStyle,
-          FontFamily,
-          Underline,
-          Image,
-          ImageResize,
-          Table,
-          TableCell,
-          TableHeader,
-          TableRow,
-          TaskItem.configure({
-            nested: true,
-          }),
-          TaskList,
-        ],
-    })
+export const Editor = () => {
+  const liveblocks = useLiveblocksExtension();
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor text",
+      },
+    },
+    extensions: [
+      liveblocks,
+      StarterKit.configure({
+        history: false,
+      }),
+      LineHeightExtension.configure({
+        types: ["heading", "paragraph"],
+        defaultLineHeight: "normal"
+      }),
+      FontSizeExtension,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https"
+
+      }),
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      TextStyle,
+      FontFamily,
+      Underline,
+      Image,
+      ImageResize,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+    ],
+  })
 
   return (
     <div className='size-full overflow-x-auto bg-[#F9FEFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
       <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
-       <EditorContent editor={editor} />
+        <EditorContent editor={editor} />
+        <Threads editor={editor} />
       </div>
     </div>
   );

b0ebad756c6075b8949ca8c0475b5fc34ce0499d
nhquang2509 <nguyenhuuquangmss@gmail.com>
2025-09-09 11:17:27 +0700

    Populate user information
    Signed-off-by: nhquang2509 <nguyenhuuquangmss@gmail.com>

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,24 +1,65 @@
-
 "use client";
 
-import { ReactNode } from "react";
+import { toast } from "sonner";
 import {
     LiveblocksProvider,
     RoomProvider,
     ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import { useParams } from "next/navigation";
+
+import { FullscreenLoader } from "@/components/fullscreen-loader";
+
+import { getUser } from "./actions";
+
+type User = { id: string; name: string; avatar: string};
 
 export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
+
+    const [users, setUser] = useState<User[]>([]);
+
+    const fetchUser = useMemo(
+        () => async () => {
+            try {
+                const list = await getUser();
+                setUser(list);
+            } catch {
+                toast.error("Failed to fetch user");
+            }
+        },
+        [],
+    );
+
+    useEffect(() => {
+        fetchUser();
+    }, [fetchUser]);
 
     return (
         <LiveblocksProvider
             throttle={16}
             authEndpoint="/api/liveblocks-auth"
+            resolveUsers={({ userIds }) => {
+                return userIds.map(
+                    (userId) => users.find((user) => user.id === userId) ?? undefined
+                )
+            }}
+            resolveMentionSuggestions={({ text }) => {
+                let filteredUsers = users;
+
+                if (text) {
+                    filteredUsers = users.filter((user) =>
+                        user.name.toLowerCase().includes(text.toLowerCase())
+                    );
+                }
+
+                return filteredUsers.map((user) => user.id);
+            }}
+            resolveRoomsInfo={() => []}
         >
             <RoomProvider id={params.documentId as string}>
-                <ClientSideSuspense fallback={<div>Loading…</div>}>
+                <ClientSideSuspense fallback={<FullscreenLoader label="Room loading..." />}>
                     {children}
                 </ClientSideSuspense>
             </RoomProvider>

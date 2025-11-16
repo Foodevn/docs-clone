df35ef05d8853e76c6c87d1ff64a96ab8b5e06b6
Foodevn <hoangphuc0918065630@gmail.com>
2025-11-16 08:44:39 +0700

    26. margin ruler

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -79,7 +79,7 @@ export function Room({ children }: { children: ReactNode }) {
                 }));
             }}
         >
-            <RoomProvider id={params.documentId as string}>
+            <RoomProvider id={params.documentId as string} initialStorage={{ leftMargin: 56, rightMargin: 56 }}>
                 <ClientSideSuspense fallback={<div>Loading room...</div>}>
                     {children}
                 </ClientSideSuspense>
